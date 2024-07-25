0a3b6dc2c031d9a630e512e2f90ad304c1489843
Dan Toledano <dan3tol99@gmail.com>
2024-07-25 12:35:33 +0300

    add eventListener + animations

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,37 +2,55 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    switch (this.innerHTML) {
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+
+  document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+  });
+
+  function makeSound(key) {
+    switch (key) {
       case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
+        let tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
         break;
       case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
+        let tom2 = new Audio("sounds/tom-2.mp3");
         tom2.play();
         break;
       case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
+        let tom3 = new Audio("sounds/tom-3.mp3");
         tom3.play();
         break;
       case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
+        let tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
         break;
       case "j":
-        var snare = new Audio("sounds/snare.mp3");
+        let snare = new Audio("sounds/snare.mp3");
         snare.play();
         break;
       case "k":
-        var crash = new Audio("sounds/crash.mp3");
+        let crash = new Audio("sounds/crash.mp3");
         crash.play();
         break;
       case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
+        let kick = new Audio("sounds/kick-bass.mp3");
         kick.play();
         break;
       default:
         console.log(this.innerHTML);
     }
-  });
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeBotton = document.querySelector("." + currentKey);
+  activeBotton.classList.add("pressed");
+  setTimeout(function () {
+    activeBotton.classList.remove("pressed");
+  }, 100);
 }
